f31560db3b98b90a99cd12dbb701d6c429f172af
Alexander Schepanovski <suor.web@gmail.com>
2012-09-30 00:29:35 +0800

    More hacks toward header recognition

--- project.clj
+++ project.clj
@@ -2,5 +2,8 @@
   :description "FIXME: write description"
   :dependencies [
     [org.clojure/clojure "1.3.0"]
-    [clj-http "0.4.3"]
-    [http.async.client "0.4.5"]])
+    [enlive "1.0.1"]
+    ; [clj-http "0.4.3"]
+    ; [http.async.client "0.4.5"]
+    ; [aleph "0.3.0-SNAPSHOT"]
+    [lamina "0.4.0-rc2"]])
--- src/grabber/core.clj
+++ src/grabber/core.clj
@@ -1,55 +1,65 @@
 (ns grabber.core
-  (:require [http.async.client :as http])
-  (:require [http.async.client.request :as request]))
+  ; (:use [lamina.core])
+  ; (:use [aleph.http])
+  (:use [net.cgrand.enlive-html :only (html-resource)])
+  )
 
 
-(defn closing [client func]
-  (fn [& args] (do
-    (http/close client)
-    (apply func args)
-  )))
+; (defn async-get [url]
+;   (async
+;     (loop [response (http-request {:method :get, :url url})]
+;       (if (contains? #{301 302 307} (:status response))
+;         (recur (http-request {:method :get, :url (get-in response [:headers "location"])}))
+;         response))))
 
-(defn async-get [url on-compete on-error]
-  (let [client (http/create-client :follow-redirects true)
-        request (request/prepare-request :get url)]
-    (request/execute-request client request
-                             :completed (closing client on-compete)
-                             :error (closing client on-error))))
+(def page (slurp "data/blog.html"))
 
-(defn async-multi [url times]
-  (let [client (http/create-client :follow-redirects true)
-        done 0]
-    (dotimes [n times]
-      (let [request (request/prepare-request :get url)]
-        (request/execute-request client request
-                                 :completed #(do % (println (str "done " n)))
-                                 :error #(println %2))))))
+(defn html-file [filename]
+  (-> filename slurp java.io.StringReader. html-resource))
 
-; This is for testing purposes only
-(defn sync-get [url on-compete on-error]
-  (let [client (http/create-client :follow-redirects true)
-        request (request/prepare-request :get url)
-        result (request/execute-request client request
-                             :completed (closing client on-compete)
-                             :error (closing client on-error))]
-        (http/await result)
-        result
-    ))
+; (defn walk [html]
+;   (tree-seq :content :content (first html)))
 
-; This is for testing purposes only
-(defn sync-get2 [url on-compete on-error]
-  (with-open [client (http/create-client :follow-redirects true)]
-    (let [request (request/prepare-request :get url)
-          result (request/execute-request client request)]
-          (http/await result)
-          result)))
+; (defn indexed [coll]
+;   (map-indexed vector coll))
 
-(defn sync-get3 [url on-compete on-error]
-  (with-open [client (http/create-client :follow-redirects true)]
-    (let [result (http/GET client url)]
-      (http/await result)
-      nil)))
+; (defn make-assoc [node]
+;   (if (associative? node) node {:value node}))
 
-(defn touch-client []
-  (let [client (http/create-client)]
-    (http/close client)))
+; (defn add-index [index node]
+;   (assoc (make-assoc node) :index index))
+
+; (defn children [node]
+;   (map-indexed add-index (:content node)))
+
+; (defn children [node]
+;   (map #(assoc (make-assoc %) :parent node) (:content node)))
+
+(defn children [node]
+  (map-indexed (fn [i el] [el {:parent node :index i}]) (:content node)))
+
+(defn walk [html]
+  (mapcat #(tree-seq (comp :content first) (comp children first) [% nil]) html))
+
+(defn nodes [coll] (map first coll))
+; (defn walk3 [html]
+;   (lazy-cat (map #(tree-seq :content :content %) html)))
+
+(defn node-value [node] (:tag node node))
+
+(defn node-values [tree]
+  (map (comp node-value first) tree))
+
+
+(defn header? [node] (#{:h1 :h2 :h3 :h4} (:tag node)))
+
+(defn formatted? [node] (#{:b :i :u :em :strong} (:tag node)))
+
+(defn text-container? [node]
+    (when-let [content (:content node)]
+        (and (= 1 (count content)) (string? (first content)))))
+
+(defn x-filter [filters tree]
+    (filter (apply every-pred (map #(comp % first) filters)) tree))
+
+; (filter (comp header? first) tree)
